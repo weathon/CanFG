b76c519eb15d08d3a60cf9b2b6bacc54e0b87d3e
Marshall <wguo6358@gmail.com>
2025-09-01 16:27:42 -0700

    Simplify CanFG loading process

--- generate_protected_paried_data.py
+++ generate_protected_paried_data.py
@@ -59,11 +59,7 @@ valid_dataloader = data.DataLoader(
 )
 
 CanFG = CanFG(args)
-# CanFG.load()
-# dict_path = 'premodels/test_irse50_seed85_anonymized_100_id_0_em_500_lp_10.pt'
-# CanFG.load(path=dict_path)
-CanFG.load_state_dict(torch.load('premodels/seed85_anonymized_100_id_0_em_500_lp_10.pt'))
-
+CanFG.load('premodels/seed85_anonymized_100_id_0_em_500_lp_10.pt')
 
 
 
